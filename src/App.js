f36c10e178d376e34f175e5946cc36199f883a03
GEBREYESUS-YONAS <148392550+GEBREYESUS-YONAS@users.noreply.github.com>
2026-04-22 10:31:47 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-aimport React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link, useNavigate } from 'react-router-dom';
 
 // Firebase Imports
